850a2172d1841aed50c576adc49ad39550c6a7fd
Samyeol Son <samyeol0930@gmail.com>
2024-01-19 16:42:11 +0900

    Chapter04: JSX 문법 정리

--- src/App.js
+++ src/App.js
@@ -2,26 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	const name = '엘리';
+	const list = ['우유', '딸기', '바나나', '요거트'];
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img
-					src={logo}
-					className='App-logo'
-					alt='logo'
-				/>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<h1 className='orange'>{`Hello! ${name}`}</h1>
+			<h2>Hello!</h2>
+			<p>{name}</p>
+			<ul>
+				{list.map((item) => (
+					<li>{item}</li>
+				))}
+			</ul>
+			<img
+				style={{ width: '200px', height: '200px' }}
+				src='https://images.unsplash.com/photo-1647984379255-c07cfbc57762?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+				alt='perfume'
+			/>
+		</>
 	);
 }
 
