c7bf86a03a48b58b57cb836dd6e7ece83d513a42
Samyeol Son <samyeol0930@gmail.com>
2024-01-19 19:07:24 +0900

    Chapter04: profile 첫 컴포넌트 만들기

---
+++ src/AppProfile.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Profile from './components/profile';
+
+function AppProfile() {
+	const name = '엘리';
+	const list = ['우유', '딸기', '바나나', '요거트'];
+	return (
+		<>
+			<Profile />
+		</>
+	);
+}
+
+export default AppProfile;
